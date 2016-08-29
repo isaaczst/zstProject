b581518d0b8daca54bf128a5010f5b097c7fbad9
Isaaczst <isaaczst@gmail.com>
2016-08-29 09:35:19 -0300

    another try

--- untitled0.py
+++ untitled0.py
@@ -5,8 +5,29 @@ Created on Sun Aug 28 10:13:58 2016
 @author: a8zn5
 """
 
-print "Name: %s \n Number: %s\n String: %s " %('dd', 'ed', 3*"_")
 
-# add non order
-a=5
-b=3
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Compute the x and y coordinates for points on sine and cosine curves
+x = np.arange(0, 3 * np.pi, 0.1)
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+
+# Set up a subplot grid that has height 2 and width 1,
+# and set the first such subplot as active.
+plt.subplot(2, 1, 1)
+
+# Make the first plot
+plt.plot(x, y_sin)
+plt.title('Sine')
+
+# Set the second subplot as active, and make the second plot.
+plt.subplot(2, 1, 2)
+plt.plot(x, y_cos)
+plt.title('Cosine')
+
+# Show the figure.
+plt.show()
+
+kk=4
